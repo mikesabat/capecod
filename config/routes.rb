856e4a0d4dba2dc2da68069eb5d23b8aef12b66b
mikesabat <mike.sabat@gmail.com>
2013-11-08 00:23:51 -0500

    Add bootstrp and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Capecod::Application.routes.draw do
+  
+  root "pages#home"
+
+  get "about" => "pages#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
